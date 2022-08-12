7c29f9d7bbed09a0501018e362b45f3b4a0dae8b
tomer9080 <tomer9080@gmail.com>
2022-08-12 11:56:46 +0300

    table for calssifiers

--- MI5_ModelTraining.py
+++ MI5_ModelTraining.py
@@ -5,9 +5,10 @@ from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QDA
 from sklearn.neighbors import KNeighborsClassifier as KNN
 from sklearn.svm import LinearSVC as SVM
 from sklearn.naive_bayes import BernoulliNB as NB
+from tabulate import tabulate
 
-# recordingFolder = "C:\BCI_RECORDINGS\\10-08-22\TK\Sub318324886"
-recordingFolder = r'C:\\Users\\Latzres\Desktop\\project\\Recordings\\10-08-22\\TK\Sub318324886002'
+recordingFolder = "C:\BCI_RECORDINGS\\10-08-22\TK\Sub318324886002"
+# recordingFolder = r'C:\\Users\\Latzres\Desktop\\project\\Recordings\\10-08-22\\TK\Sub318324886002'
 
 # recordingFolder = "C:\BCI_RECORDINGS\\09-08-22\RL\Sub316353903001"
 
@@ -18,100 +19,82 @@ label_train = sio.loadmat(recordingFolder + '\LabelTrain.mat')['LabelTrain'].rav
 features_test = sio.loadmat(recordingFolder + '\FeaturesTest.mat')['FeaturesTest'] 
 label_test = sio.loadmat(recordingFolder + '\LabelTest.mat')['LabelTest'].ravel()
 
-print(features_train.shape, label_train.shape, features_test.shape, label_test.shape)
-
 
 #LDA analysis
-print("starting LDA analysis: \n")
+print("Running LDA analysis...")
 lda_predictor = LDA()
 lda_predictor.fit(features_train, label_train)
 lda_prediction = lda_predictor.predict(features_test)
-
-print("LDA predction: \n")
-print(lda_prediction)
-print("LDA label test: \n")
-print(label_test)
-
 test_results = lda_prediction - label_test
-print("test results: \n")
-print(sum(test_results == 0)/len(label_test))
+hit_rate = sum(test_results == 0)/len(label_test)
+
+lda_row = ['LDA', hit_rate, lda_prediction, label_test] 
+
 
 #QDA analysis
-print("starting QDA analysis: \n")
+print("Running QDA analysis...")
 qda_predictor = QDA()
 qda_predictor.fit(features_train, label_train)
 qda_prediction = qda_predictor.predict(features_test)
-
-print("QDA predction: \n")
-print(qda_prediction)
-print("QDA label test: \n")
-print(label_test)
-
 test_results = qda_prediction - label_test
-print("test results: \n")
-print(sum(test_results == 0)/len(label_test))
+hit_rate = sum(test_results == 0)/len(label_test)
+
+qda_row = ['QDA', hit_rate, qda_prediction, label_test] 
+
 
 #KNN analysis
 
 #KNN-5
-print("starting KNN-5 analysis: \n")
+print("Running KNN-5 analysis...")
 knn_predictor = KNN(5)
 knn_predictor.fit(features_train, label_train)
 knn_prediction = knn_predictor.predict(features_test)
-
-print("KNN-5 predction: \n")
-print(knn_prediction)
-print("KNN-5 label test: \n")
-print(label_test)
-
 test_results = knn_prediction - label_test
-print("test results: \n")
-print(sum(test_results == 0)/len(label_test))
+hit_rate = sum(test_results == 0)/len(label_test)
+
+knn_5_row = ['KNN-5', hit_rate, knn_prediction, label_test] 
 
 #KNN-7
-print("starting KNN-7 analysis: \n")
+print("Running KNN-7 analysis...")
 knn_predictor = KNN(7)
 knn_predictor.fit(features_train, label_train)
 knn_prediction = knn_predictor.predict(features_test)
-
-print("KNN-7 predction: \n")
-print(knn_prediction)
-print("KNN-7 label test: \n")
-print(label_test)
-
 test_results = knn_prediction - label_test
-print("test results: \n")
-print(sum(test_results == 0)/len(label_test))
+hit_rate = sum(test_results == 0)/len(label_test)
 
+knn_7_row = ['KNN-7', hit_rate, knn_prediction, label_test] 
 
 #SVM analysis
 # Need to check convergence problem.
-print("starting SVM analysis: \n")
+print("Running SVM analysis...")
 svm_predictor = SVM(penalty='l2', loss='hinge', multi_class='ovr', C=2, max_iter=30_000)
 svm_predictor.fit(features_train, label_train)
 svm_prediction = svm_predictor.predict(features_test)
-
-print("SVM predction: \n")
-print(svm_prediction)
-print("SVM label test: \n")
-print(label_test)
-
 test_results = svm_prediction - label_test
-print("test results: \n")
-print(sum(test_results == 0)/len(label_test))
+hit_rate = sum(test_results == 0)/len(label_test)
+
+svm_row = ['SVM', hit_rate, svm_prediction, label_test] 
 
 
 #NB analysis
-print("starting NB analysis: \n")
+print("Running NB analysis...")
 nb_predictor = NB()
 nb_predictor.fit(features_train, label_train)
 nb_prediction = nb_predictor.predict(features_test)
-
-print("NB predction: \n")
-print(nb_prediction)
-print("NB label test: \n")
-print(label_test)
-
 test_results = nb_prediction - label_test
-print("test results: \n")
-print(sum(test_results == 0)/len(label_test))
+hit_rate = sum(test_results == 0)/len(label_test)
+
+nb_row = ['NB', hit_rate, nb_prediction, label_test] 
+
+#### ---------- Priniting table ---------- ####
+print('')
+headers = ["Classifier", "Success Rate", "Classifier Prediction", "Test Labels"]
+all_rows = [
+    lda_row,
+    qda_row,
+    knn_5_row,
+    knn_7_row,
+    svm_row,
+    nb_row
+]
+print(tabulate(all_rows, headers=headers))
