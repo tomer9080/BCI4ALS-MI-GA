1367500ebea2abac1faec57cd5a783b38898a2c2
roilatzres <roilatzres@gmail.com>
2022-08-17 11:03:02 +0300

    17-08 run

--- MainScript.m
+++ MainScript.m
@@ -19,8 +19,8 @@ clc; clear; close all;
 %disp('Finished stimulation and EEG recording. Stop the LabRecorder and press any key to continue...');
 %pause;
 
-recordingFolder = 'C:\Users\Latzres\Desktop\project\Recordings\16-08-22\TK\Sub318324886002';
-% recordingFolder = 'C:\Users\Latzres\Desktop\project\Recordings\15-08-22\RL\Sub316353903001';
+% recordingFolder = 'C:\Users\Latzres\Desktop\project\Recordings\17-08-22\TK\Sub318324886002';
+recordingFolder = 'C:\Users\Latzres\Desktop\project\Recordings\17-08-22\RL\Sub316353903002';
 % recordingFolder = 'C:\Users\Latzres\Desktop\project\Recordings\11-08-22\Sub20220811003';
 % recordingFolder = 'C:\Users\Latzres\Desktop\project\Recordings\16-08-22\RL\Sub316353903002';
 
