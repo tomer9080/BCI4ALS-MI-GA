e54929adc211d1aab0775caa39cfd67428c09e29
roilatzres <roilatzres@gmail.com>
2022-09-05 19:25:14 +0300

    folders etc

--- MainScript.m
+++ MainScript.m
@@ -23,10 +23,12 @@ addpath '.\FeatureExtraction\'
 
 % recordingFolder = 'C:\Users\Latzres\Desktop\project\Recordings\30-08-22\TK\Sub318324886002';
 % recordingFolder = 'C:\Users\Latzres\Desktop\project\Recordings\30-08-22\RL\Sub316353903003';
-recordingFolder = 'C:\Users\Latzres\Desktop\project\Recordings\16-08-22\TT\Sub20220816004';
+% recordingFolder = 'C:\Users\Latzres\Desktop\project\Recordings\16-08-22\TT\Sub20220816004';
 
 
-recordingFolder = 'C:\BCI_RECORDINGS\15-08-22\TK\Sub318324886002';
+recordingFolder = 'C:\Users\Latzres\Desktop\project\Recordings\yonatan\Sub313552119'
+
+% recordingFolder = 'C:\BCI_RECORDINGS\15-08-22\TK\Sub318324886002';
 % recordingFolder = 'C:\BCI_RECORDINGS\22-08-22\RL\Sub316353903001';
 
 %% Run pre-processing pipeline on recorded data
